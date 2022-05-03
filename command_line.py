f136144a70aac5143813f7506b9dab697193543b
Tom <tom.fleming@live.co.uk>
2022-05-03 14:27:00 +0100

    first commit

---
+++ command_line.py
@@ -0,0 +1,26 @@
+# Command Line
+
+# print working directory - pwd
+# list current directory - ls
+# change directory - cd <__>
+# change up a directory - cd . .
+# make a new directory - mkdir
+# print command - echo <__> | <__>
+# preview - cat <__>
+# remove - rm -r <__>
+
+# Initialise Git - git init <__>
+# Set Name - git config -global user.name "<__>"
+# Set Email - got config -global user.email "<__>"
+# Add to staging area - git add <__>
+# Commit changes - git commit -m "<__>"
+# List Branches - git branch -a
+# New Branch - git branch <__>
+# Merge Branches - git merge <__>
+# Check staging area - git status
+# Check logs - git log
+# Push to GitHub - git push -u <__> <__>
+
+# Initial set-up - gh auth login
+# Create Repository gh repo create < > [—flags]
+# Search Repository gh search repos < > [—flags]
