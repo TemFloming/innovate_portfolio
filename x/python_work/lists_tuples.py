ff6cfba0601d6e3d5f545a372094b1eef61c5e39
Tom <tom.fleming@live.co.uk>
2022-05-06 16:55:12 +0100

    fourth commit

---
+++ x/python_work/lists_tuples.py
@@ -0,0 +1,88 @@
+# Lists and Tuples
+
+# list1 = [
+#     1, 2, 3
+
+# ]
+
+# tuple1 = (
+#     1, 2, 3
+# )
+
+# coffee_order = [
+#     "Will - black americano",
+#     "Katy - cappucino"
+# ]
+
+# print(coffee_order[0])
+
+# coffee_order[0] = "Will - doppio espresso"
+
+# print(coffee_order[0])
+
+# for i in coffee_order:
+#     print(i)
+
+# coffee_tuple = (
+#     "Nick - mocha",
+#     "Sebastian - hot chocolate"
+# )
+
+# for i in coffee_tuple:
+#     print (i)
+
+# coffee_tuple[0] = "Nick - decaf mocha"
+
+# list_of_lists = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+
+# print(list_of_lists[1][0]
+
+# )
+
+# even_nums = [2, 4, 6, 8, 10]
+
+# odd_nums = (1, 3, 5, 7, 9)
+
+# even_nums.append(12)
+# even_nums.insert(2, 0)
+
+# for i in even_nums:
+#     print(i)
+
+
+# odd_nums = (1, 3, 5, 7, 9)
+# odd_nums.remove(1)
+# odd_nums.pop()
+
+# odd_nums += (11,13,15)
+# print(odd_nums)
+
+# list_of_fruit = [
+#     "apples",
+#     "bananas",
+#     "oranges",
+#     "pears",
+#     "grapes",
+#     "pineapples",
+#     "star-fruit",
+#     "mango",
+#     "cheeries",
+#     "black-berries"
+
+# ]
+
+# print(list_of_fruit[0:10:2])
+
+# nums = [0,1,2,3,4,5,6,7,8,9,10]
+
+# print(nums[0:11:3])
+
+# num = 0 
+
+# while num < 10:
+#     print(num)
+#     num+=1
+
---
+++ x/python_work/morepython.py
@@ -0,0 +1,74 @@
+# print("morepython")
+
+# print(int(5.4)) 
+
+# print(int("54"))
+
+# # print(int("ghdfgjofjgojf"))
+
+# print(float(54))
+
+# # casting
+
+# print(type(str(54)))
+# print(type(str(5.4)))
+
+# print("What is your name?")
+# name=input()
+
+# if name:
+#     print(f"Hello {name}.")
+# else:
+#     print("You did not provide a name.")
+
+# print(not True)
+
+# print(not False)
+
+# bool = False
+
+# if bool != True:
+#     print (False)
+# else:
+#     print(True)
+
+# def add_up():
+#     num1 = input("First number to add?")
+#     num2 = input("Second number to add?")
+    # print(int(num1) + int(num2))
+
+    # try:
+    #     print(f"{num1} + {num2} is {int(num1) + int(num2)}")
+
+    # except:
+    #     print("Error!")
+    #     print("************")
+    #     add_up()
+
+# add_up()
+
+# light = False
+
+# def light_switch():
+#     global light
+#     if light:
+#         print("Whoah! It's bright in here!")
+#         light - False
+#     else:
+#         print("Who turned out the lights?")
+#         light = True
+
+# light_switch()
+# light_switch()
+
+# balance = 100
+
+# def cash_withdraw(amount):
+#     global balance
+#     print(f"Your balance is {balance}.")
+#     print(f"You are withdrawing {amount}.")
+#     balance -=amount
+#     print(f"Your new balance is {balance}.")
+
+# cash_withdraw(20)
+
